918ff03e28803c5d47676c990a77f13d2b43eb80
Lucas <lucasvalenteds@gmail.com>
2021-12-12 04:05:10 -0300

    Implementing TurnOn command

---
+++ src/main/java/com/example/command/commands/TurnOn.java
@@ -0,0 +1,12 @@
+package com.example.command.commands;
+
+import com.example.command.contract.CommandName;
+import com.example.command.contract.CommandPayload;
+
+public final class TurnOn extends GenericCommand<CommandPayload.Empty, CommandPayload.Empty> {
+
+    @Override
+    public CommandName getName() {
+        return CommandName.TurnOn;
+    }
+}
