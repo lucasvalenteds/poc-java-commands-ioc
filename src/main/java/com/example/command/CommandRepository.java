be5d870890104e983d25bbbf16ff8a8d7e9a9fb2
Lucas <lucasvalenteds@gmail.com>
2021-12-14 19:48:43 -0300

    Clarifying exceptions thrown in CommandRepository

--- src/main/java/com/example/command/CommandRepository.java
+++ src/main/java/com/example/command/CommandRepository.java
@@ -2,7 +2,7 @@ package com.example.command;
 
 import com.example.command.contract.CommandResult;
 import com.example.command.exceptions.CommandNotFoundException;
-import org.springframework.dao.DataAccessException;
+import com.example.command.exceptions.CommandPersistenceException;
 
 import java.util.UUID;
 
@@ -10,9 +10,9 @@ public interface CommandRepository {
 
     CommandResult findById(UUID id) throws CommandNotFoundException;
 
-    void save(CommandResult.Processed result) throws DataAccessException;
+    void save(CommandResult.Processed result) throws CommandPersistenceException;
 
-    void save(CommandResult.Persisted result) throws DataAccessException;
+    void save(CommandResult.Persisted result) throws CommandPersistenceException;
 
-    void save(CommandResult.Executed result) throws DataAccessException;
+    void save(CommandResult.Executed result) throws CommandPersistenceException;
 }
